6258d08dc9b06563a682d8afe2183863afa8c825
Giulia <51050879+iamgiu@users.noreply.github.com>
2022-03-06 17:10:34 +0100

    Update 7.c

--- Esercizi/7.c
+++ Esercizi/7.c
@@ -32,3 +32,38 @@ int main(void) {
 
   return 0;
 }
+
+
+
+
+
+
+
+
+
+/* Soluzione alternativa proposta esercizio "Valore assoluto" */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+  int a ; /* numero inserito ed il corrispondente valore assoluto*/
+
+  /* LEGGI IL NUMERO */
+  printf("Immetti un numero: ") ;
+  scanf("%d", &a) ;
+
+  /* STAMPA IL NUMERO */
+  printf("Il numero inserito e’ %d\n", a) ;
+
+  /* VERIFICA SE IL NUMERO E’ NEGATIVO */
+  if ( a < 0 ){
+    /* SE IL NUMERO E’ NEGATIVO, IL VALORE ASSOLUTO E’ OTTENUTO CAMBIANDO
+    IL SEGNO DEL NUMERO */
+    a = -a ;
+  }
+  /* STAMPA IL RISULTATO */
+  printf("Il valore assoluto del numero inserito e’ %d\n", a) ;
+
+  exit(0) ;
+}
