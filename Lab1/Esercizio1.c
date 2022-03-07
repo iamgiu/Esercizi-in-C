5f4b38eac4bac4cb170606ee0ff5db426a4b1ccc
Giulia <51050879+iamgiu@users.noreply.github.com>
2022-03-07 17:13:07 +0100

    Add files via upload

---
+++ Lab1/Esercizio1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+  int x, y;
+  float z;
+
+  printf("Insert an integer number: ");
+  scanf("%d", &x);
+
+  y = 3;
+
+  z = (float)(x) / y;
+
+  printf("%d/%d = %.3f\n", x, y, z);
+
+  return 0;
+}
